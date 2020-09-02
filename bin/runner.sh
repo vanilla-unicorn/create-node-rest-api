226377d15603bb10c12caa9f419caba9fba82616
vanilla-unicorn <vanilla_unicorn@via.tokyo.jp>
2020-09-02 23:34:50 +0900

    ADDED: version increment and conditional statement attempt to try and provide conditional installation of dependencies based on macos or linux host

--- bin/runner.sh
+++ bin/runner.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 mkdir $1
-cp -a /usr/local/lib/node_modules/create-node-rest-api/template/. $1 || cp -a /usr/lib/node_modules/create-node-rest-api/template/. $1
+if cp -a /usr/local/lib/node_modules/create-node-rest-api/template/. $1;
+then
+    echo "Installing project dependencies"
+elif cp -a /usr/lib/node_modules/create-node-rest-api/template/. $1;
+then
+    echo "Installing project dependencies"
+else
+    echo "Unable to install project dependencies and initialize project"
+fi
 cd $1
 yarn install
