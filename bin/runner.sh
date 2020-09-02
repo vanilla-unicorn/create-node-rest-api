e70395d6c60131d718ca797cf2e588fd2e60bf5c
vanilla-unicorn <vanilla_unicorn@via.tokyo.jp>
2020-09-02 23:23:32 +0900

    ADDED: support for linux location of package when generating a new project

--- bin/runner.sh
+++ bin/runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir $1
-cp -a /usr/local/lib/node_modules/create-node-rest-api/template/. $1
+cp -a /usr/local/lib/node_modules/create-node-rest-api/template/. $1 || cp -a /usr/lib/node_modules/create-node-rest-api/template/. $1
 cd $1
 yarn install
